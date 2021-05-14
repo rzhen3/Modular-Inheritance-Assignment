cde840940417b75a01da34eb92051761b63f4e5e
RoyZ <royzheng2003@gmail.com>
2021-05-13 22:49:28 -0400

    hello world

---
+++ src/modular/and/inheritance/assignment/ModularAndInheritanceAssignment.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modular.and.inheritance.assignment;
+
+/**
+ *
+ * @author Roy Zheng
+ */
+
+public class ModularAndInheritanceAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world");
+    }
+    
+}
