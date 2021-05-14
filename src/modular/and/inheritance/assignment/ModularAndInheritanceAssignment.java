3ee2c51cd5333ef347f11059534a8cbd1cd651f0
RoyZ <royzheng2003@gmail.com>
2021-05-13 22:49:55 -0400

    hello github

--- src/modular/and/inheritance/assignment/ModularAndInheritanceAssignment.java
+++ src/modular/and/inheritance/assignment/ModularAndInheritanceAssignment.java
@@ -18,6 +18,7 @@ public class ModularAndInheritanceAssignment {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        System.out.println("Hello github");
     }
     
 }
